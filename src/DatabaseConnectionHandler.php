474be28861f6d6127171d2ddc7dc8b4487e0cf65
Moritz Kanzler <mka@netkotec.de>
2021-03-18 16:41:44 +0100

    adding transaction functionality

--- src/DatabaseConnectionHandler.php
+++ src/DatabaseConnectionHandler.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace MK\DB;
 
 use \PDO;
 
-class DatabaseConnectionHandler {
+class DatabaseConnectionHandler
+{
 
     /**
      * Undocumented variable
@@ -18,9 +20,10 @@ class DatabaseConnectionHandler {
      * @return DatabaseConnectionHandler
      * @throws DatabaseConnectionException
      */
-    public static function getInstance($db_server = null, $db_name = null, $db_user = null, $db_password = null) {
+    public static function getInstance($db_server = null, $db_name = null, $db_user = null, $db_password = null)
+    {
         static $instance = null;
-        if($instance === null) {
+        if ($instance === null) {
             try {
                 $instance = new DatabaseConnectionHandler($db_server, $db_name, $db_user, $db_password);
             } catch (DatabaseConnectionException $e) {
@@ -34,12 +37,13 @@ class DatabaseConnectionHandler {
      * Undocumented function
      * @throws DatabaseConnectionException
      */
-    private function __construct($db_server, $db_name, $db_user, $db_password) {
+    private function __construct($db_server, $db_name, $db_user, $db_password)
+    {
         try {
-            if($db_server === null || $db_name === null || $db_user === null || $db_password === null) {
+            if ($db_server === null || $db_name === null || $db_user === null || $db_password === null) {
                 throw new DataBaseConnectionException("MySQL login credentials not found");
             }
-            $this->pdo = new PDO('mysql:host='.$db_server.';dbname='.$db_name.';charset=utf8', $db_user, $db_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+            $this->pdo = new PDO('mysql:host=' . $db_server . ';dbname=' . $db_name . ';charset=utf8', $db_user, $db_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
             $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
@@ -55,22 +59,33 @@ class DatabaseConnectionHandler {
      * @return DatabaseResult
      * @throws Exception
      */
-    public function query($query, $arguments = array()) {
+    public function query($query, $arguments = array())
+    {
         try {
-            $statement = $this->pdo->prepare($query);        
+            $statement = $this->pdo->prepare($query);
             $statement->execute($arguments);
             $last_insert_id = $this->pdo->lastInsertId();
             $db_result = new DatabaseResult($statement, $last_insert_id);
             return $db_result;
         } catch (\PDOException $e) {
-            throw new DatabaseQueryException($e->getMessage(), 0 , $e);
+            throw new DatabaseQueryException($e->getMessage(), 0, $e);
         }
     }
 
-    function __destruct() {
-        $this->pdo = null;
+    public function doTransaction($callback)
+    {
+        try {
+            $this->pdo->beginTransaction();
+            $callback($this->pdo);
+            $this->pdo->commit();
+        } catch (\PDOException $e) {
+            $this->pdo->rollBack();
+            throw new DatabaseTransactionException($e->getMessage(), 0, $e);
+        }
     }
 
-
-
+    function __destruct()
+    {
+        $this->pdo = null;
+    }
 }
---
+++ src/DatabaseTransactionException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace MK\DB;
+
+class DatabaseTransactionException extends \Exception
+{
+}
